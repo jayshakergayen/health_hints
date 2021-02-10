665826d4fd1e75640edcb2471b62d22306d65555
jayshakergayen <grouchydragon92@gmail.com>
2021-02-10 10:10:12 -0500

    More options and changed gradient bg

--- js/main.js
+++ js/main.js
@@ -19,8 +19,17 @@ function my_function() {
       "Write a Poem",
       "Clean up your room and make your bed (I like to listen to music while I do it)",
       "Yoga (the best medicine)",
-      "Listeno to music",
-      "Learn something new"
+      "Listen to music",
+      "Learn something new",
+      "Draw or paint something",
+      "Create a bucket list to complete",
+      "Watch a movie, tv show, etc. that you want to watch",
+      "Think about what you would do if you were stuck in your house the whole day",
+      "Take a small nap",
+      "Bake cookies, cake, brownies, etc. (it's not that hard)",
+      "Go into a forest/park and try to spot as many animals as you can, write them down (make sure to be careful)",
+      "Count how many countries you've been too and count the ones you want to go to",
+      'Code something like print "Hello World"'
     
     ];
     
